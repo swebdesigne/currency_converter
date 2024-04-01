4f74eafe00d92f7a6663227d13dccc9ef135ae51
swebdesigne <swebdesigne@gmail.com>
2024-04-01 17:22:00 +0300

    Добавлена Spring service discouvery (сервис обнаружения сервисов)

--- exchange-processing/src/main/java/com/currency/exchange/ExchangeProcessingApplication.java
+++ exchange-processing/src/main/java/com/currency/exchange/ExchangeProcessingApplication.java
@@ -2,8 +2,10 @@ package com.currency.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories("com.currency.exchange.repository")
 public class ExchangeProcessingApplication {
--- src/main/java/com/currency/converter/CurrencyConverterApplication.java
+++ src/main/java/com/currency/converter/CurrencyConverterApplication.java
@@ -2,9 +2,10 @@ package com.currency.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-//@EnableConfigurationProperties(CurrencyClientConfig.class)
 public class CurrencyConverterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class);
